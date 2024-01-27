6e63eb9913d4984dbc8ee9ff83cb35ce65c207e6
bintojoseph <70394879+bintojoseph@users.noreply.github.com>
2024-01-27 22:24:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import Home from './components/Home'
+import About from './components/About'
+import Programs from './components/Programs'
+import Highlights from './components/Highlights'
+import Mentors from './components/Mentors'
+import Faq from './components/Faq'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>hello</h1>
+      <Home/>
+      <About/>
+      <Programs/>
+      <Highlights/>
+      <Mentors/>
+      <Faq/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
