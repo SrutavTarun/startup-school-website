dfe0fb60cc9c3c338ff68ffc5d243fa269840f3b
fbn776 <fbn776@gmail.com>
2024-01-28 08:31:25 +0530

    Basic setups

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import "../styles/faq.css"
 
 export default function Faq() {
   return (
-    <div>Faq</div>
+  <>
+    <h1 className="text-center text-ttOrange h1_font font-semibold">FAQ</h1>
+    <h2 className="text-center h2_font font-bold mx-24 ">Learn more about through us FAQ</h2>
+
+    <section>
+      
+    </section>
+  </>
   )
 }
+
