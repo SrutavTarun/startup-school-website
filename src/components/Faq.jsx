b19bfc86b2f04773db4c294983c541878eb5fa94
fbn776 <fbn776@gmail.com>
2024-01-28 11:32:39 +0530

    Responsive changes

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -5,8 +5,8 @@ import faqData from "../data/faqData.js";
 export default function Faq() {
   return (
   <>
-    <h1 className="text-center text-ttOrange h1_font font-semibold">FAQ</h1>
-    <h2 className="text-center h2_font font-bold mx-24 ">Learn more about through us FAQ</h2>
+    <h1 className="h1_font">FAQ</h1>
+    <h2 className="h2_font">Learn more about through us FAQ</h2>
     <br/>
     <section>
       <Accordion data={faqData}/>
