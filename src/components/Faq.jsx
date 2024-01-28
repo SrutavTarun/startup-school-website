54c00e98fd755801042021f542b94f8bbea3846d
fbn776 <fbn776@gmail.com>
2024-01-28 11:19:41 +0530

    Added Accordions

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,13 +1,15 @@
 import "../styles/faq.css"
+import Accordion from "./utils/Accordion.jsx";
+import faqData from "../data/faqData.js";
 
 export default function Faq() {
   return (
   <>
     <h1 className="text-center text-ttOrange h1_font font-semibold">FAQ</h1>
     <h2 className="text-center h2_font font-bold mx-24 ">Learn more about through us FAQ</h2>
-
+    <br/>
     <section>
-      
+      <Accordion data={faqData}/>
     </section>
   </>
   )
---
+++ src/data/faqData.js
@@ -0,0 +1,24 @@
+const faqData = [
+	{
+		question: 'Question1',
+		answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel urna ege tincidunt, ultricies nunc quis, aliquam nisl.'
+	},
+	{
+		question: 'Question2',
+		answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel urna ege tincidunt, ultricies nunc quis, aliquam nisl.'
+	},
+	{
+		question: 'Question3',
+		answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel urna ege tincidunt, ultricies nunc quis, aliquam nisl.'
+	},
+	{
+		question: 'Question4',
+		answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel urna ege tincidunt, ultricies nunc quis, aliquam nisl.'
+	},
+	{
+		question: 'Question5',
+		answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel urna ege tincidunt, ultricies nunc quis, aliquam nisl.'
+	}
+]
+
+export default faqData;
