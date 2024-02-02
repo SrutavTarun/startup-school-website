62f83137d2273fd1f805af57a414106e30bdcca9
Srutav Tarun <srutavtarun@gmail.com>
2024-02-02 07:28:42 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./styles/home.css";
+
+import backgroundImage from "../assets/home1.png";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div
+        className="home w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div>
+          <div className="text-black text-center font-SpaceGrotesk font-bold text-4xl">
+          From Beginner to Founder: Join Startup School for a transformative journey.
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
