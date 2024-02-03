a7f6c5842e4c7256016db1cd2bb83738ade1cf61
bintojoseph <70394879+bintojoseph@users.noreply.github.com>
2024-02-03 14:39:30 +0530

    data update

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,12 @@ import MentorsNew from './components/MentorsNew'
 import Faq from './components/Faq'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import {client} from './client'
-import {DataContext} from './context'
 import './App.css'
 import Highlights from './components/Highlights'
 
 
 
 function App() {
-  const { mentors, setMentorData } =
-    useContext(DataContext);
-
-    useEffect(() => {
-      
-      const fetchData = async () => {
-  
-        //mentors
-        await client
-          .fetch(`*[_type == "mentors"] | order(_createdAt desc)`)
-          .then((data) => {
-            // console.log(data);
-            setMentorData(data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
-      fetchData();
-    }, []);
 
   return (
     <>
